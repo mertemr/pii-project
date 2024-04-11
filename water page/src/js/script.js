aaf4d591580e1863cf0663cc166d70ef15bf014d
Mert Emir <mertemir38@hotmail.com>
2024-04-11 19:35:42 +0300

    reformatted web-src files

--- water page/src/js/script.js
+++ water page/src/js/script.js
@@ -1,25 +1,25 @@
-const timeline = document.querySelector('#timeline');
-const waterBtn = document.querySelector('#waterBtn');
-const settingBtn = document.querySelector('#settingBtn');
-const settingDetail = document.querySelector('#settingDetail');
+const timeline = document.querySelector("#timeline");
+const waterBtn = document.querySelector("#waterBtn");
+const settingBtn = document.querySelector("#settingBtn");
+const settingDetail = document.querySelector("#settingDetail");
 
 let time = 0;
 
-settingBtn.addEventListener('click',()=>{
-    if(settingDetail.classList.contains('hidden')){
-        settingDetail.classList.remove('hidden');
-    }else{
-        settingDetail.classList.add('hidden');
-    }
+settingBtn.addEventListener("click", () => {
+  if (settingDetail.classList.contains("hidden")) {
+    settingDetail.classList.remove("hidden");
+  } else {
+    settingDetail.classList.add("hidden");
+  }
 });
 
-waterBtn.addEventListener('click',()=>{
-    if(time != 0){
-        time=0;
-    }
+waterBtn.addEventListener("click", () => {
+  if (time != 0) {
+    time = 0;
+  }
 });
 
-setInterval(()=>{
-    time += 1;
-    timeline.innerHTML =`${time}`
-},1000);
+setInterval(() => {
+  time += 1;
+  timeline.innerHTML = `${time}`;
+}, 1000);
