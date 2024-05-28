916f96bb26f794313d320760b75e1838d88e6760
mertemr <48881002+mertemr@users.noreply.github.com>
2024-05-28 10:42:48 +0300

    fix typo

--- web-interface/server.js
+++ web-interface/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
-const routes = require("./routes/routes");
+const router = require("./routes/router");
 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Started at http://localhost:3000");
